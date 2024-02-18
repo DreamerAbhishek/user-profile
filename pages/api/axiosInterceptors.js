ad73a9538f6a1d705df1e83038aa0d805ac8a869
Abhishek Verma <abhishek.verma_5669@gmail.com>
2024-02-18 17:28:13 +0530

    first commit

---
+++ pages/api/axiosInterceptors.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { API_URL } from "./config";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
---
+++ pages/api/config.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://jsonplaceholder.typicode.com";
+
+export const DICEBEAR_API_URL = "https://api.dicebear.com/7.x/initials/svg";
